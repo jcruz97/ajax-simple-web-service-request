2fd13b5eec9eb0c6597dcc962eb26f29f7e0d110
jcruz97 <cruzjurgen97@gmail.com>
2020-07-23 10:28:39 +0200

    first push

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+    let xhr = new XMLHttpRequest;
+
+    xhr.open('GET', 'https://api.github.com/users', true)
+
+    xhr.onload = function() 
+        {
+            if (this.status === 200) 
+                {
+                    console.log(JSON.parse(this.responseText));
+        }
+                }
+    xhr.send();
+
+    function quoting (){
+        document.getElementById("quote").innerHTML ="lul";
+    }
